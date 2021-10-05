303ce9b9e1c3913bf9b06919bd540febc2081d17
Hichami Houssam <hichami.houssam@gmail.com>
2021-10-05 18:09:19 +0100

    fixing linter errors

--- src/crud.js
+++ src/crud.js
@@ -1,24 +1,24 @@
 export const list = document.querySelector('.score-list');
 export const scores = JSON.parse(localStorage.getItem('scoreList')) || [];
 
-export let createScore = (name, score) => {
+export const createScore = (name, score) => {
   const scoreInfo = document.createElement('li');
 
   scoreInfo.innerHTML = `<div class="name">${name} : </div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<div class="score">${score}</div>`;
   list.appendChild(scoreInfo);
-}
+};
 
-export let loadDom = () => {
+export const loadDom = () => {
   scores.forEach((score) => {
     createScore(score.name, score.score);
   });
-}
+};
 
-export let AddScore = (scoreArr) => {
+export const AddScore = (scoreArr) => {
   const score = {
     name: document.getElementById('name').value,
     score: document.getElementById('score').value,
   };
   scoreArr.push(score);
   localStorage.setItem('scoreList', JSON.stringify(scoreArr));
-}
+};
